dc41134fe19ad78974b9a6f0b9b120c264b1e978
unknown <gtluu912@gmail.com>
2023-01-17 14:55:52 -0800

    file updates

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='gtluu/CCSP2.0',
+    version='0.1.0',
+    packages=['gtluu/CCSP2.0'],
+    url='https://github.com/gtluu/CCSP2.0',
+    author='Gordon T. Luu',
+    author_email='goluu@ucsc.edu',
+    description='ommand line version of CCS Predictor 2.0 forked from the Facundo Lab'
+)
